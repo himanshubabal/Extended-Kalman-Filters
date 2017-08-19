78ceaf9811f89f3bd7b63e3553a78df734afc405
Himanshu Babal <babal.himanshu@gmail.com>
2017-08-19 17:00:42 +0530

    normalised pi

--- CarND-Extended-Kalman-Filter-Project-master/src/kalman_filter.cpp
+++ CarND-Extended-Kalman-Filter-Project-master/src/kalman_filter.cpp
@@ -1,5 +1,7 @@
 #include "kalman_filter.h"
+#include <string>
 
+using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -9,68 +11,82 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
   /**
     * predict the state
   */
-  x_ = F_ * x_;
-  MatrixXd Ft = F_.transpose();
-  P_ = F_ * P_ * Ft + Q_;
+    x_ = F_ * x_;
+    MatrixXd Ft = F_.transpose();
+    P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
     * update the state by using Kalman Filter equations
   */
-  VectorXd z_pred = H_ * x_;
-  VectorXd y = z - z_pred;
-  MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
-  MatrixXd PHt = P_ * Ht;
-  MatrixXd K = PHt * Si;
+    VectorXd z_pred = H_ * x_;
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd K = P_ * Ht * S.inverse();
 
-  //new estimate
-  x_ = x_ + (K * y);
-  long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * H_) * P_;
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
     * update the state by using Extended Kalman Filter equations
   */
-  // Convert from cartesian to polar
-  float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
-  float phi = atan2(x_(1), x_(0));
-  float rho_dot;
-  if (fabs(rho) < 0.0001) {
-    rho_dot = 0;
-  } else {
-    rho_dot = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
-  }
-  VectorXd z_pred(3);
-  z_pred << rho, phi, rho_dot;
-  VectorXd y = z - z_pred;
-  MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
-  MatrixXd PHt = P_ * Ht;
-  MatrixXd K = PHt * Si;
+    // Convert from cartesian to polar
+    // Convert from cartesian to polar
+    float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
 
-  //new estimate
-  x_ = x_ + (K * y);
-  long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * H_) * P_;
+    if (x_(0) == 0 && x_(1) == 0){
+        throw string("atan2(0.0,0.0) is undefined");
+    }
+    float phi = atan2(x_(1), x_(0));
+    float rho_dot;
+    if (fabs(rho) < 0.0001) {
+    rho_dot = 0;
+    } else {
+    rho_dot = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
+    }
+
+    VectorXd h_x(3);
+    h_x << rho, phi, rho_dot;
+
+    // Normalise y, make phi in -pi to +pi
+    VectorXd y = z - h_x;
+
+    // Keeping phi above -pi
+    while(y[1] < -M_PI){
+        y[1] += 2 * M_PI;
+    }
+    // Keeping phi below +pi
+    while(y[1] > M_PI){
+        y[1] -= 2 * M_PI;
+    }
+
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd K = P_ * Ht * S.inverse();
+
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 
 }
